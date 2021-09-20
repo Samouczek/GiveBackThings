9cf3281d91e4e122c14a2e26dcf43af6952c98c6
Samouczek <795kasia@gmail.com>
2021-09-20 18:18:31 +0200

    Add style HomeSimpleSteps component

--- src/components/homePage/HomeSimpleSteps.js
+++ src/components/homePage/HomeSimpleSteps.js
@@ -4,35 +4,37 @@ import {LOG_IN} from "../../constants/Route";
 export default function HomeSimpleSteps() {
     return (
         <div className='container'>
-            <h2>Wystarczą 4 proste kroki</h2>
-            <div className='image-decoration'> </div>
-            <div className='row'>
-                <div className='col-lg-2'> </div>
-                <div className='step step--first col-lg-2 col-md-3 col-sm-12'>
-                    <p className='step-title'>Wybierz rzeczy</p>
-                    <div className='step-line'> </div>
-                    <p className='step-text'>ubrania, zabawki, sprzęt i inne</p>
+            <div className='steps-section'>
+                <h2 className='steps-title'>Wystarczą 4 proste kroki</h2>
+                <div className='image-steps-decoration'> </div>
+                <div className='row steps'>
+                    <div className='col-lg-2'> </div>
+                    <div className='step step--first col-lg-2 col-md-3 col-sm-12'>
+                        <p className='step-title'>Wybierz rzeczy</p>
+                        <div className='step-line'>          </div>
+                        <p className='step-text'>ubrania, zabawki, sprzęt i inne</p>
+                    </div>
+                    <div className='step step--second col-lg-2 col-md-3 col-sm-12'>
+                        <p className='step-title'>Spakuj je</p>
+                        <div className='step-line'> </div>
+                        <p className='step-text'>skorzystaj z worków na śmieci</p>
+                    </div>
+                    <div className='step step--third col-lg-2 col-md-3 col-sm-12'>
+                        <p className='step-title'>Zdecyduj komu chcesz pomóc</p>
+                        <div className='step-line'> </div>
+                        <p className='step-text'>wybierz zaufane miejsce</p>
+                    </div>
+                    <div className='step step--fourth col-lg-2 col-md-3 col-sm-12'>
+                        <p className='step-title'>Zamów kuriera</p>
+                        <div className='step-line'> </div>
+                        <p className='step-text'>kurier przyjedzie w dogodnym terminie</p>
+                    </div>
+                    <div className='col-lg-2'> </div>
                 </div>
-                <div className='step step--second col-lg-2 col-md-3 col-sm-12'>
-                    <p className='step-title'>Spakuj je</p>
-                    <div className='step-line'> </div>
-                    <p className='step-text'>skorzystaj z worków na śmieci</p>
-                </div>
-                <div className='step step--third col-lg-2 col-md-3 col-sm-12'>
-                    <p className='step-title'>Zdecyduj komu chcesz pomóc</p>
-                    <div className='step-line'> </div>
-                    <p className='step-text'>wybierz zaufane miejsce</p>
-                </div>
-                <div className='step step--fourth col-lg-2 col-md-3 col-sm-12'>
-                    <p className='step-title'>Zamów kuriera</p>
-                    <div className='step-line'> </div>
-                    <p className='step-text'>kurier przyjedzie w dogodnym terminie</p>
-                </div>
-                <div className='col-lg-2'> </div>
+                <button className='btn-steps'>
+                    <Link to={LOG_IN} >oddaj rzeczy</Link>
+                </button>
             </div>
-            <button className='btn-home btn-home_header'>
-                <Link to={LOG_IN} >oddaj rzeczy</Link>
-            </button>
         </div>
     )
 }

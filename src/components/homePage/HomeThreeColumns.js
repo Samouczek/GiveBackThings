00e675c8758c07e08014b4cce0cd16caefd38355
Samouczek <795kasia@gmail.com>
2021-09-20 14:52:55 +0200

    Add HomeTreeColumns component

--- src/components/homePage/HomeThreeColumns.js
+++ src/components/homePage/HomeThreeColumns.js
@@ -1,7 +1,33 @@
 
 export default function HomeThreeColumns() {
     return (
-        <>
-        </>
+        <div className='container'>
+            <div className='three-columns row'>
+                <div className='column col-md-4 col-sm-12'>
+                    <p className='column-number'>10</p>
+                    <p className='column-title'>oddanych worków</p>
+                    <p className='column-text'>
+                        Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra
+                        elementuma. Aliquam erat volutpat.
+                    </p>
+                </div>
+                <div className='column col-md-4 col-sm-12'>
+                    <p className='column-number'>5</p>
+                    <p className='column-title'>wspartych organizacji</p>
+                    <p className='column-text'>
+                        Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra
+                        elementuma. Aliquam erat volutpat.
+                    </p>
+                </div>
+                <div className='column col-md-4 col-sm-12'>
+                    <p className='column-number'>7</p>
+                    <p className='column-title'>zorganizowanych zbiórek</p>
+                    <p className='column-text'>
+                        Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra
+                        elementuma. Aliquam erat volutpat.
+                    </p>
+                </div>
+            </div>
+        </div>
     )
 }

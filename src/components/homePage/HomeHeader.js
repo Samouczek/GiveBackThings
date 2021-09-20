ec20b82faf8cc77346ca18e9303e864aeb59f53c
Samouczek <795kasia@gmail.com>
2021-09-20 14:41:49 +0200

    Add approximate header styl

--- src/components/homePage/HomeHeader.js
+++ src/components/homePage/HomeHeader.js
@@ -7,22 +7,25 @@ export default function HomeHeader() {
     return (
         <div className='container header'>
             <div className='row'>
-                <div className='col-md-6'>
-                    <img src='../../assets/Home-Hero-Image.jpg' alt='box with things' />
+                <div className='col-md-6 image-header'>
                 </div>
-                <div className='col-md-6 col-sm-12'>
+                <div className='col-md-6 col-sm-12 right-header'>
                     <Navigation />
                     <h1>
                         Zacznij pomagać! <br />
                         Oddaj niechciane rzeczy w zaufane ręce
                     </h1>
-                    <img src='../../assets/Decoration.svg' alt='decoration' />
-                    <button className='btn-home btn-home_header'>
-                        <Link to={LOG_IN} component={Login}>oddaj rzeczy</Link>
-                    </button>
-                    <button className='btn-home btn-home_header'>
-                        <Link to={LOG_IN} component={Login}>zorganizuj zbiórkę</Link>
-                    </button>
+                    <div className='image-decoration'>
+                    </div>
+                    <div className='btns-header'>
+                        <button className='btn-home btn-home_header'>
+                            <Link to={LOG_IN} >oddaj rzeczy</Link>
+                        </button>
+                        <button className='btn-home btn-home_header'>
+                            <Link to={LOG_IN} >zorganizuj zbiórkę</Link>
+                        </button>
+                    </div>
+
                 </div>
             </div>
         </div>

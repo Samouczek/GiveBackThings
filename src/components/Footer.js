9302bc3615ee65c0e8a455e91be0575d1709b547
Samouczek <795kasia@gmail.com>
2021-09-21 17:23:15 +0200

    Add HomeFooter component style

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,9 @@ export default function Footer() {
     return (
         <div className='container footer'>
             <div className='row footer-info'>
-                <div className='copyright col-sm-10'>Copyright by Coders Lab</div>
-                <div className='media col-sm-2'>  </div>
+                <div className='copyright col-sm-8 col-md-10 col-lg-11'>Copyright by Coders Lab</div>
+                <div className='media col-sm-4 col-md-2 col-lg-1'>
+                </div>
             </div>
         </div>
     )

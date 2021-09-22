145648445d4dd976c2b70ecd7aa8b1bd64bd38d3
Samouczek <795kasia@gmail.com>
2021-09-22 15:51:34 +0200

    Add file with constants for institutions

---
+++ src/constants/Institution.js
@@ -0,0 +1,38 @@
+
+const foundations = [
+      ["Fundacja “Dbam o Zdrowie”", "Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej.",
+          "ubrania, jedzenie, sprzęt AGD, meble, zabawki"],
+      ["Fundacja “Dla dzieci”","Cel i misja: Pomoc dzieciom z ubogich rodzin.",
+          "ubrania, meble, zabawki"],
+        ["Fundacja “Bez domu”", "Cel i misja: Pomoc dla osób nie posiadających miejsca zamieszkania.",
+        "ubrania, jedzenie, ciepłe koce"],
+        ["Fundacja Zbieramy razem", "Cel i misja: Pomoc dla osób niepełnosprawnych lub ciężko chorych",
+            "ubrania, jedzenie, sprzęt AGD, pieniądze"],
+        ["Fundacja “Mimo wszytsko”","Cel i misja: Pomoc dla osób  dorosłych z niepełnosprawnością intelektualną. ",
+            "ubrania, meble, sprzęt AGD"],
+        ["Fundacja Chcę żyć", "Cel i misja: Pomoc dla dzieci chorych na trudno uleczalne choroby.",
+            "ubrania, zabawki, jedzenie"],
+        ["Fundacja “Dzieci.org”", "Cel i misja: Pomoc dla dzieci i młodzieży niepełnospprawnej.",
+            "ubrania, jedzenie, sprzęt AGD, meble, zabawki"],
+        ["Fundacja Dziecięce Marzenia","Cel i misja: Pomoc dzieciom z ubogich rodzin.",
+            "ubrania, meble, zabawki, jedzenie"],
+        ["Fundacja Ekologicznych Rozwiązań", "Cel i misja: Podniesienie świadomości ekologicznej społeczeństwa.",
+            "zainteresowanie ludzi"]
+];
+
+const nonGovernmentalOrganizations = [
+    ["Organizacja “Lorem Ipsum 1”", "Quis varius quam quisque id diam vel elementum pulvinar.","Egestas, sed, tempus"],
+    ["Organizacja  “Lorem Ipsum 2”", "Hendrerit gravida rutrum quisque non tellus orci ac auctor augue.","Ut, aliquam, purus, sit, amet"],
+    ["Organizacja  “Lorem Ipsum 3”", "Scelerisque in dictum non consectetur a erat nam.","Mi, quis, hendrerit, dolor"],
+    ["Organizacja “Lorem Ipsum 4”", "Quis varius quam quisque id diam vel elementum pulvinar.","Egestas, sed, tempus"],
+    ["Organizacja  “Lorem Ipsum 5”", "Hendrerit gravida rutrum quisque non tellus orci ac auctor augue.","Ut, aliquam, purus, sit, amet"],
+    ["Organizacja  “Lorem Ipsum 6”", "Scelerisque in dictum non consectetur a erat nam.","Mi, quis, hendrerit, dolor"]
+];
+
+const localCollection = [
+    ["Zbiórka “Lorem Ipsum 1”", "Quis varius quam quisque id diam vel elementum pulvinar.","Egestas, sed, tempus"],
+    ["Zbiórka “Lorem Ipsum 2”", "Hendrerit gravida rutrum quisque non tellus orci ac auctor augue.","Ut, aliquam, purus, sit, amet"],
+    ["Zbiórka “Lorem Ipsum 3”", "Scelerisque in dictum non consectetur a erat nam.","Mi, quis, hendrerit, dolor"]
+];
+
+export default {foundations, nonGovernmentalOrganizations, localCollection};

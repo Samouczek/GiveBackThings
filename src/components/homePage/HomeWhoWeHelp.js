0283835b188c259bcdaafa77b0f68362ca6a63d8
Samouczek <795kasia@gmail.com>
2021-09-22 13:59:22 +0200

    Add HomeWhoWeHelp component style in progress and small fixes

--- src/components/homePage/HomeWhoWeHelp.js
+++ src/components/homePage/HomeWhoWeHelp.js
@@ -8,14 +8,14 @@ export default function HomeWhoWeHelp() {
         <Element name="whoWeHelpElement">
             <div className='container who-we-help'>
                 <div className='help-section'>
-                    <h2 className='help-title'>Komu pomagamy?</h2>
+                    <h2 className='help__title'>Komu pomagamy?</h2>
                     <div className='decoration decoration--who-we-help'> </div>
-                    <div className='institutions'>
+                    <div className='help__institutions'>
                         <button className='btn-institution'>Fundacjom</button>
                         <button className='btn-institution'>Organizacjom pozarządowym</button>
                         <button className='btn-institution'>Lokalnym zbiórkom</button>
                     </div>
-                    <p className='institutions-info'>
+                    <p className='help__institutions-info'>
                         W naszej bazie znajdziesz listę zweryfikowanych Fundacji, z którymi współpracujemy.
                         Możesz sprawdzić czym się zajmują, komu pomagają i czego potrzebują.
                     </p>

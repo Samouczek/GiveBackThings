bea82b7d6ed1a824e1abdf0ddc16acb07bac8734
Samouczek <795kasia@gmail.com>
2021-09-23 12:30:53 +0200

    InstitutionsDisplay component styled

--- src/components/atoms/InstitutionsDisplay.js
+++ src/components/atoms/InstitutionsDisplay.js
@@ -1,40 +1,41 @@
-import {useState} from "react";
 
 function InstitutionsDisplay({data}) {
 
     return (
                 <div className='display-institutions'>
-                    <div className='display-institutions__left'>
                         {data[0].length === 3 &&
-                        <div className='institution'>
-                            <p className='institution--name'> {data[0][0]} </p>
-                            <p className='institution--purpose'> {data[0][1]} </p>
-                        </div>
+                            <div className='institution'>
+                                <div className='institution-data'>
+                                    <p className='institution--name'> {data[0][0]} </p>
+                                    <p className='institution--purpose'> {data[0][1]} </p>
+                                </div>
+                                <div className='institution--needs'>
+                                    <p className='institution--needs--paragraph'>{data[0][2]}</p>
+                                </div>
+                            </div>
                         }
                         {data[1].length === 3 &&
-                            <div className='institution'>
+                        <div className='institution'>
+                            <div className='institution-data'>
                                 <p className='institution--name'>{data[1][0]}</p>
                                 <p className='institution--purpose'>{data[1][1]}</p>
                             </div>
+                            <div className='institution--needs'>
+                                <p className='institution--needs--paragraph'>{data[1][2]}</p>
+                            </div>
+                        </div>
                         }
                         {data[2].length === 3 &&
-                            <div className='institution'>
+                        <div className='institution'>
+                            <div className='institution-data'>
                                 <p className='institution--name'>{data[2][0]}</p>
                                 <p className='institution--purpose'>{data[2][1]}</p>
                             </div>
+                            <div className='institution--needs'>
+                                <p className='institution--needs--paragraph'>{data[2][2]}</p>
+                            </div>
+                        </div>
                         }
-                    </div>
-                    <div className='display-institutions__right'>
-                        {data[0].length === 3 &&
-                            <div className='institution--needs'>{data[0][2]}</div>
-                        }
-                        {data[1].length === 3 &&
-                            <div className='institution--needs'>{data[1][2]}</div>
-                        }
-                        {data[2].length === 3 &&
-                            <div className='institution--needs'>{data[2][2]}</div>
-                        }
-                    </div>
                 </div>
     )
 }

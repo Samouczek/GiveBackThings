405d6d05aaa69720fe6f7e9eeb50e8c5cdb842f7
Samouczek <795kasia@gmail.com>
2021-09-24 10:47:01 +0200

    Add form validatio - second condition

--- src/logic/FormValidation.js
+++ src/logic/FormValidation.js
@@ -1,6 +1,6 @@
 
 export const nameValidation = (name) => {
-    if (name.includes(" ")){
+    if (name.includes(" ") || name.length === 0){
         return "Podane imię jest nieprawidłowe";
     }
     return 0;
@@ -8,7 +8,7 @@ export const nameValidation = (name) => {
 
 export const emailValidation = (email) => {
     const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if (!re.test(email)) {
+    if (!re.test(email) || email.length === 0) {
         return "Podany email jest nieprawidłowy"
     }
    return 0;

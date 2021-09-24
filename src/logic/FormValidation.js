bff4342bc7b128fbdc84a8832f1dd756d092ed29
Samouczek <795kasia@gmail.com>
2021-09-24 10:42:49 +0200

    Add form validation

---
+++ src/logic/FormValidation.js
@@ -0,0 +1,22 @@
+
+export const nameValidation = (name) => {
+    if (name.includes(" ")){
+        return "Podane imię jest nieprawidłowe";
+    }
+    return 0;
+}
+
+export const emailValidation = (email) => {
+    const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!re.test(email)) {
+        return "Podany email jest nieprawidłowy"
+    }
+   return 0;
+}
+
+export const messageValidation = (message) => {
+    if (message.length < 120) {
+        return "Wiadomość musi mieć conajmniej 120 znaków";
+    }
+    return 0;
+}

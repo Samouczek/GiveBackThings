b43f7ef313832b6175e5f0b731f6783bdeb38881
Samouczek <795kasia@gmail.com>
2021-09-24 18:20:40 +0200

    Add style of Registration and Login component

--- src/components/registrationPage/Registration.js
+++ src/components/registrationPage/Registration.js
@@ -4,7 +4,7 @@ import {LOG_IN, REGISTRATION} from "../../constants/Route";
 
 function Registration() {
     return (
-        <div className='container login'>
+        <div className='container registration-section'>
             <div className='row'>
                 <div className='col-lg-6 left-empty'>
                 </div>
@@ -17,27 +17,29 @@ function Registration() {
                 <div className='decoration login__decoration'>
                 </div>
                 <form className='login__form'>
-                    <div className="login__form-field">
-                        <label className="login__form-label" >
-                            Email
-                            <input type="email" className="login__form-input"/>
-                        </label>
-                    </div>
-                    <div className="login__form-field">
-                        <label className="login__form-field">
-                            Hasło
-                            <input type="password" className="login__form-input"/>
-                        </label>
-                    </div>
-                    <div className="login__form-field">
-                        <label className="login__form-field">
-                            Powtórz hasło
-                            <input type="password" className="login__form-input"/>
-                        </label>
+                    <div className='login__form-fields'>
+                        <div className="login__form-field">
+                            <label className="login__form-label" >
+                                Email
+                                <input type="email" className="login__form-input"/>
+                            </label>
+                        </div>
+                        <div className="login__form-field">
+                            <label className="login__form-label">
+                                Hasło
+                                <input type="password" className="login__form-input"/>
+                            </label>
+                        </div>
+                        <div className="login__form-field">
+                            <label className="login__form-label">
+                                Powtórz hasło
+                                <input type="password" className="login__form-input"/>
+                            </label>
+                        </div>
                     </div>
                     <div className='btn login__btn'>
-                        <button><Link to={LOG_IN} >Zaloguj się</Link></button>
-                        <input type="submit" value='Załóż konto' className="btn btn--submit"/>
+                        <button className='left-btn'><Link to={LOG_IN} >Zaloguj się</Link></button>
+                        <input type="submit" value='Załóż konto' className="btn right-btn"/>
                     </div>
                 </form>
             </div>

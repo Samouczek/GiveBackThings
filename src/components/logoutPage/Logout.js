4c447dddf6b433c9954265ff53ede0d03e27f98a
Samouczek <795kasia@gmail.com>
2021-09-27 11:34:19 +0200

    Add structur and style of Logout component

--- src/components/logoutPage/Logout.js
+++ src/components/logoutPage/Logout.js
@@ -1,8 +1,25 @@
+import Navigation from "../Navigation";
+import {Link} from "react-router-dom";
+import {HOME, REGISTRATION} from "../../constants/Route";
 
 function Logout() {
     return (
-        <>
-        </>
+        <div className='container logout-section'>
+            <div className='row'>
+                <div className='col-lg-6 left-empty'> </div>
+                <div className='col-lg-6 col-sm-12 right-nav'>
+                    <Navigation />
+                </div>
+            </div>
+            <div className='logout'>
+                <h1 className='logout__header'>Wylogowanie nastąpiło <br/>
+                        pomyślnie!</h1>
+                <div className='decoration logout__decoration'> </div>
+                <button className='main-page-btn'>
+                    <Link to={HOME}>Strona główna</Link>
+                </button>
+            </div>
+        </div>
     )
 }
 

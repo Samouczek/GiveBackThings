5ffd7075bf2ec4d3b92e4b8b78bdbb8365af6db9
Samouczek <795kasia@gmail.com>
2021-09-28 20:48:19 +0200

    Component ThingsFirstStep - almost styled

--- src/components/HandOverThingsPage/ThingsFirstStep.js
+++ src/components/HandOverThingsPage/ThingsFirstStep.js
@@ -2,28 +2,29 @@
 function ThingsFirstStep() {
     return (
         <div className='container first-step-section'>
-            <div className='row'>
+            <div className='row first-step'>
                 <div className='first-step-left col-lg-6 col-sm-12'>
                     <div className='number-step'>Krok 1/4</div>
                     <form className='first-step-form'>
+                        <h2 className='first-step-form-title'>Zaznacz co chcesz oddać:</h2>
                         <label className='first-step-label'>
-                            <input className='first-step-input' type="radio" value="option1" />
+                            <input className='first-step-input' type="radio" value="option1" name="things"/>
                             ubrania, które nadają się do ponownego użycia
                         </label>
                         <label className='first-step-label'>
-                            <input className='first-step-input' type="radio" value="option1" />
+                            <input className='first-step-input' type="radio" value="option2" name="things"/>
                             ubrania, do wyrzucenia
                         </label>
                         <label className='first-step-label'>
-                            <input className='first-step-input' type="radio" value="option1" />
+                            <input className='first-step-input' type="radio" value="option3" name="things"/>
                             zabawki
                         </label>
                         <label className='first-step-label'>
-                            <input className='first-step-input' type="radio" value="option1" />
+                            <input className='first-step-input' type="radio" value="option4" name="things"/>
                             książki
                         </label>
                         <label className='first-step-label'>
-                            <input className='first-step-input' type="radio" value="option1" />
+                            <input className='first-step-input' type="radio" value="option5" name="things"/>
                             Inne
                         </label>
                     </form>

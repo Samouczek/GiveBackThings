998d4ef61eff0d77b38a074d8d31531ad608955f
Samouczek <795kasia@gmail.com>
2021-09-28 12:09:32 +0200

    add linking form facebook and instagram icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,6 +5,8 @@ export default function Footer() {
             <div className='row footer-info'>
                 <div className='copyright col-sm-8 col-md-10 col-lg-11'>Copyright by Coders Lab</div>
                 <div className='media col-sm-4 col-md-2 col-lg-1'>
+                   <a href='https://www.facebook.com/' className='media media__fb'> <img src="../assets/Facebook.svg" alt='facebook'/></a>
+                    <a href='https://www.instagram.com/' className='media media__insta'> <img src="../assets/Instagram.svg" alt='instagram'/></a>
                 </div>
             </div>
         </div>

861f77a1211072e1c49b907a88900aec2193a1aa
Samouczek <795kasia@gmail.com>
2021-09-28 22:00:24 +0200

    Component ThingsSecondStep - almost styled cd

--- src/components/HandOverThingsPage/ThingsSecondStep.js
+++ src/components/HandOverThingsPage/ThingsSecondStep.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function ThingsSecondStep() {
+    const [choice, setChoice] = useState(false);
     return (
         <div className='container second-step-section'>
             <div className='row background-step'>
@@ -10,7 +11,7 @@ function ThingsSecondStep() {
                         <h2 className='step-form-title'>Podaj liczbę 60l worków, w które spakowałeś/aś rzeczy:</h2>
                         <label className='second-step-label'>
                             Liczba 60l worków:
-                            <select value='-wybierz-'>
+                            <select value={choice} className='second-step-select' >
                                 <option value="1">1</option>
                                 <option value="2">2</option>
                                 <option value="3">3</option>

dced7eb5183578da4dfb3477a064a8a57d9373e0
Samouczek <795kasia@gmail.com>
2021-09-28 18:54:35 +0200

    Component ThingsImportantNotice - add style

--- src/components/HandOverThingsPage/ThingsHeader.js
+++ src/components/HandOverThingsPage/ThingsHeader.js
@@ -2,8 +2,8 @@ import Navigation from "../Navigation";
 
 function ThingsHeader() {
     return (
-        <div className='container things_header'>
-            <div className='row'>
+        <div className='container '>
+            <div className='row things_header'>
                 <div className='col-lg-6 image-things-header'>
                 </div>
                 <div className='col-lg-6 col-sm-12 right-header'>
@@ -14,20 +14,20 @@ function ThingsHeader() {
                         </h1>
                         <div className='decoration decoration--header'> </div>
                         <h2 className='things_subtitle'>Wystarczą 4 proste kroki:</h2>
-                        <div className='things_steps'>
-                            <div className='things_step'>
+                        <div className='things_steps row'>
+                            <div className='things_step col-sm-6'>
                                 <p className='things_step_number'>1</p>
                                 <p className='things_step_title'>Wybierz <br/> rzeczy</p>
                             </div>
-                            <div className='things_step'>
+                            <div className='things_step  col-sm-6'>
                                 <p className='things_step_number'>2</p>
                                 <p className='things_step_title'>Spakuj je <br/> w worki</p>
                             </div>
-                            <div className='things_step'>
+                            <div className='things_step  col-sm-6'>
                                 <p className='things_step_number'>3</p>
                                 <p className='things_step_title'>Wybierz <br/> fundację</p>
                             </div>
-                            <div className='things_step'>
+                            <div className='things_step  col-sm-6'>
                                 <p className='things_step_number'>4</p>
                                 <p className='things_step_title'>Zamów <br/> kuriera</p>
                             </div>
--- src/components/HandOverThingsPage/ThingsImportantNotice.js
+++ src/components/HandOverThingsPage/ThingsImportantNotice.js
@@ -1,8 +1,12 @@
 
 function ThingsImportantNotice(){
     return (
-        <>
-        </>
+        <div className='container important-section'>
+            <div className='important-content'>
+                <h2 className='important-title'>Ważne!</h2>
+                <p  className='important-text'>Uzupełnij szczegóły dotyczące Twoich rzeczy. Dzięki temu będziemy wiedzieć komu najlepiej je przekazać.</p>
+            </div>
+        </div>
     )
 }
 

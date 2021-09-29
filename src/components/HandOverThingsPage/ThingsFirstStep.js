7f2e3433a68a1546d23eb2e8b34b05f206c4b7f6
Samouczek <795kasia@gmail.com>
2021-09-29 22:36:15 +0200

    Component ThingsFirstStep - styled

--- src/components/HandOverThingsPage/ThingsFirstStep.js
+++ src/components/HandOverThingsPage/ThingsFirstStep.js
@@ -2,8 +2,13 @@ import {useEffect, useState} from "react";
 
 function ThingsFirstStep({thingsDonate, onDoneThings, onDoneStep}) {
    const [things, setThings] = useState(thingsDonate);
+   const [firstChoice, setFirstChoice] = useState(false);
+    const [secondChoice, setSecondChoice] = useState(false);
+    const [thirdChoice, setThirdChoice] = useState(false);
+    const [fourthChoice, setFourthChoice] = useState(false);
+    const [fifthChoice, setFifthChoice] = useState(false);
 
-   useEffect(() => {
+    useEffect(() => {
        if (things !== false) {
            if (typeof onDoneThings === 'function'){
                onDoneThings(things);
@@ -20,6 +25,19 @@ function ThingsFirstStep({thingsDonate, onDoneThings, onDoneStep}) {
        }
     }
 
+    const handleOnChange = (e,number) => {
+        setThings(e.target.value);
+        setFirstChoice(false);
+        setSecondChoice(false);
+        setThirdChoice(false);
+        setFourthChoice(false);
+        setFifthChoice(false);
+        (number===1) && setFirstChoice(true);
+        (number===2) && setSecondChoice(true);
+        (number===3) && setThirdChoice(true);
+        (number===4) && setFourthChoice(true);
+        (number===5) && setFifthChoice(true);
+    }
     return (
         <div className='container first-step-section'>
             <div className='row background-step'>
@@ -28,52 +46,67 @@ function ThingsFirstStep({thingsDonate, onDoneThings, onDoneStep}) {
                     <form className='first-step-form'>
                         <h2 className='step-form-title'>Zaznacz co chcesz oddać:</h2>
                         <label className='first-step-label'>
+                            <div className='click-input'>
+                                {firstChoice && <div className='click-checked'> </div>}
+                            </div>
                             <input
                                 className='first-step-input'
                                 type="radio"
                                 value="ubrania, które nadają się do ponownego użycia"
                                 name="things"
-                                onChange={e =>  setThings(e.target.value)}
+                                onChange={e => handleOnChange(e,1)}
                             />
                             ubrania, które nadają się do ponownego użycia
                         </label>
                         <label className='first-step-label'>
+                            <div className='click-input'>
+                                {secondChoice && <div className='click-checked'> </div>}
+                            </div>
                             <input
                                 className='first-step-input'
                                 type="radio"
                                 value="ubrania, do wyrzucenia"
                                 name="things"
-                                onChange={e =>  setThings(e.target.value)}
+                                onChange={e => handleOnChange(e,2)}
                             />
                             ubrania, do wyrzucenia
                         </label>
                         <label className='first-step-label'>
+                            <div className='click-input'>
+                                {thirdChoice && <div className='click-checked'> </div>}
+                            </div>
                             <input
                                 className='first-step-input'
                                 type="radio"
                                 value="zabawki"
                                 name="things"
-                                onChange={e =>  setThings(e.target.value)}
+                                onChange={e => handleOnChange(e,3)}
                             />
                             zabawki
                         </label>
                         <label className='first-step-label'>
+                            <div className='click-input'>
+                                {fourthChoice && <div className='click-checked'> </div>}
+                            </div>
                             <input
                                 className='first-step-input'
                                 type="radio"
                                 value="książki"
                                 name="things"
-                                onChange={e =>  setThings(e.target.value)}
+                                onChange={e => handleOnChange(e,4)}
                             />
                             książki
                         </label>
                         <label className='first-step-label'>
+                            <div className='click-input'>
+                                {fifthChoice && <div className='click-checked'> </div>}
+                            </div>
                             <input
                                 className='first-step-input'
                                 type="radio"
                                 value="inne"
                                 name="things"
-                                onChange={e =>  setThings(e.target.value)}
+                                onChange={e => handleOnChange(e,5)}
                             />
                             inne
                         </label>

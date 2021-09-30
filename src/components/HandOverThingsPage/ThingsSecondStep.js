171f8801d55c1b6eb351640c358685d2a0c51b77
Samouczek <795kasia@gmail.com>
2021-09-30 15:29:46 +0200

    Component ThingsSecondStep - select option work

--- src/components/HandOverThingsPage/ThingsSecondStep.js
+++ src/components/HandOverThingsPage/ThingsSecondStep.js
@@ -9,44 +9,51 @@ function ThingsSecondStep({numberBags,onDoneBags,onDoneStep}) {
                 onDoneBags(bags);
             }
     })
-
     const handleOnClickNext = (e) => {
         e.preventDefault();
             if (typeof onDoneStep === 'function'){
                 onDoneStep(3);
             }
     }
-
     const handleOnClickBack = (e) => {
         e.preventDefault();
         if (typeof onDoneStep === 'function'){
             onDoneStep(1);
         }
     }
+    const handleOnClickSelect = () => {
+        setOpenCheckBox(prev => !prev);
+    }
+    const handleOnClickOption = (number) => {
+        setBags(number);
+        setOpenCheckBox(prev => !prev);
+    }
 
     return (
         <div className='container second-step-section'>
             <div className='row background-step'>
-                <div className='second-step-left col-lg-6 col-sm-12'>
+                <div className='second-step-left col-sm-12'>
                     <div className='number-step'>Krok 2/4</div>
                     <form className='second-step-form'>
                         <h2 className='step-form-title'>Podaj liczbę 60l worków, w które spakowałeś/aś rzeczy:</h2>
                         <div className='second-step-label'>
                             <p className='second-step-label-title'>Liczba 60l worków:</p>
                             <div className="select-heading">
-                                <div className="select-title">
+                                <div className="select-title" onClick={handleOnClickSelect}>
                                     <div className="select-title--title"> wybierz </div>
                                     {openCheckBox ?
                                         <div className="select-up-arrow"> </div> : <div className="select-down-arrow"> </div>
                                     }
                                 </div>
-                                <ul className='select-options'>
-                                    <li className="select-option">1</li>
-                                    <li className="select-option">2</li>
-                                    <li className="select-option">3</li>
-                                    <li className="select-option">4</li>
-                                    <li className="select-option">5</li>
-                                </ul>
+                                {openCheckBox &&
+                                    <ul className='select-options'>
+                                        <li className="select-option" onClick={(number)=>handleOnClickOption(1)}>1</li>
+                                        <li className="select-option" onClick={(number)=>handleOnClickOption(2)}>2</li>
+                                        <li className="select-option" onClick={(number)=>handleOnClickOption(3)}>3</li>
+                                        <li className="select-option" onClick={(number)=>handleOnClickOption(4)}>4</li>
+                                        <li className="select-option" onClick={(number)=>handleOnClickOption(5)}>5</li>
+                                    </ul>
+                                }
                             </div>
                         </div>
                     </form>
@@ -55,7 +62,7 @@ function ThingsSecondStep({numberBags,onDoneBags,onDoneStep}) {
                         <button className='btn-step btn-next'onClick={handleOnClickNext}>Dalej</button>
                     </div>
                 </div>
-                <div  className='first-step-right col-lg-6 col-sm-12'>
+                <div  className='first-step-right col-sm-12'>
                 </div>
             </div>
         </div>

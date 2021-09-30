3a55078e7965bad0ad4024d5c4f065c6aa942acb
Samouczek <795kasia@gmail.com>
2021-09-30 16:26:53 +0200

    Create CreateHelpGroups function

---
+++ src/logic/CreateHelpGroups.js
@@ -0,0 +1,12 @@
+
+function CreateHelpGroups(firstCheckbox, secondCheckbox,thirdCheckbox, fourthCheckbox,fifthCheckbox){
+    const helpGroups = [];
+    firstCheckbox && helpGroups.push("dzieciom");
+    secondCheckbox && helpGroups.push("samotnym matkom");
+    thirdCheckbox && helpGroups.push("bezdomnym");
+    fourthCheckbox && helpGroups.push("niepełnosprawnym");
+    fifthCheckbox && helpGroups.push("osobom starszym");
+    return helpGroups;
+}
+
+export default CreateHelpGroups; 
